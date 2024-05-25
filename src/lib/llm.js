52170c55b47844fbc360c068b284b8d9e3253516
kwangyy <70025553+kwangyy@users.noreply.github.com>
2024-05-25 14:06:09 +0800

    testing llm.js

---
+++ src/lib/llm.js
@@ -0,0 +1,17 @@
+const OpenAI = require("openai");
+const dotenv = require("dotenv");
+
+dotenv.config({path: "../../.env"});
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-4o",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
