b296325205453d892915f91f8006c98af14ddd96
maquilino.2022 <mikhailaquilino@gmail.com>
2024-05-25 14:28:34 +0800

    implemented barebones feature cards with routing

---
+++ src/components/ui/dashboard_card.tsx
@@ -0,0 +1,24 @@
+import {
+    Card,
+    CardTitle,
+} from "./card";
+import { Button } from "./button";
+import Link from "next/link";
+
+interface props {
+    title: string,
+    href: string,
+}
+
+const DashboardCard: React.FC<props> = ({title, href}) => {
+    return (
+        <Card className={`w-48 h-72 flex flex-col items-center justify-center relative`}>
+            <CardTitle>{title}</CardTitle>
+            <Button variant="outline" className="absolute bottom-4">
+                <Link href={href}>Find out more</Link>
+            </Button>
+        </Card>
+    );
+}
+
+export default DashboardCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,26 @@
+/**
+ * mik:
+ * i think we can just make the home page the dashboard page. quite likely the user will just
+ * navigate to where they want from there, then its one less layer of hierarchy for navigation also
+ * 
+ * feature_X will be replaced with the actual feature names later
+ */
+
 import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import DashboardCard from "@/components/ui/dashboard_card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-xl lg:flex">
-        This is the home page if we even need it
-      </div>
-      <Button variant={"outline"} onClick={() => console.log("dashbard button clicked")}>
-        <Link href="/dashboard">Dashboard Page</Link>
-      </Button>
-    </main>
-  );
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
+        >
+            <div className="flex gap-10">
+              <DashboardCard title="Feature 1" href="/feature_1"/>
+              <DashboardCard title="Feature 2" href="/feature_2"/>
+              <DashboardCard title="Feature 3" href="/feature_3"/>
+            </div>
+        </main>
+    );
 }
