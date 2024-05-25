311ebf8ca08da1942a0dc3638a8f7bfe86a1711a
maquilino.2022 <mikhailaquilino@gmail.com>
2024-05-26 01:55:05 +0800

    code cleanup

--- src/components/ui/investment_alert.tsx
+++ src/components/ui/investment_alert.tsx
@@ -2,9 +2,10 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AlertCircle } from "lucide-react";
 import { useState } from "react";
 import { Button } from "./button";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 export default function InvestmentAlert() {
-    const [isShown, setIsShown] = useState(false);
+    const [isShown, setIsShown] = useState(true);
 
     const toggleShow = () => {
         setIsShown(!isShown);
@@ -13,8 +14,10 @@ export default function InvestmentAlert() {
         <div className="fixed inset-0 flex items-center justify-center z-50">
             <Alert className={`w-3/5 h-[250px] flex flex-col items-center transform transition-transform duration-500 ${isShown ? 'translate-y-0' : 'translate-y-[26.5rem]'}`}>
                 <div className="flex w-full justify-between">
-                    <AlertCircle className="h-4 w-4"/>
-                    <Button className="w-4 h-8" onClick={toggleShow}/>
+                    <AlertCircle className="h-8 w-8"/>
+                    <Button className="w-3 h-8 text-xs" onClick={toggleShow}>
+                        {isShown ? "Hide" : "Show"}
+                    </Button>
                 </div>
                 <AlertTitle className="mt-8">Warning</AlertTitle>
                 <AlertDescription>
