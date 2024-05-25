fe2bc40e3be84e7919f51d7e2b6795f1d0f54fb1
sheng <chengzhisheng2001@gmail.com>
2024-05-25 13:26:49 +0800

    feat: add avatar

--- src/components/chatbot/Chat.tsx
+++ src/components/chatbot/Chat.tsx
@@ -1,4 +1,15 @@
-export function ChatbotMessage() {}
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+export function ChatbotMessage() {
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src='https://github.com/shadcn.png' />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+}
 
 export function Chatbot() {
   return <div> chatbot</div>;
