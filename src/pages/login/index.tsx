b2278d2b938a182289af659b279113283e2184f2
maquilino.2022 <mikhailaquilino@gmail.com>
2024-05-26 11:33:30 +0800

    redirect to singpass login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Login() {
     return (
@@ -6,7 +7,9 @@ export default function Login() {
 
             <h2 className="flex flex-row justify-center items-center text-xl pb-4">Welcome back!</h2>
             <Button className="bg-red-500 rounded-sm p-4">
-                Login via Singpass
+                <Link href="https://login.singpass.gov.sg/main?scope=openid&response_type=code&redirect_uri=https://www.cpf.gov.sg/eservices/broker/sp/interaction/callback&state=eyJzdGF0ZSI6ImxDUWVfRkczblkxZW5wS3ZQMEFxTUFSN2NpT04xTU9aaVFhaGNpbGVzQm8ifQ%3D%3D&nonce=ydWLJ8W7jMq6eZtFyCFADiWyzoPJkezXmwgqYOuFM34&client_id=yojGKYotEMq2uTFj2fLTTTdNxDaqPeQ3&pwd_enabled=true">
+                    Login via Singpass
+                </Link>
             </Button>
         </div>
     )
